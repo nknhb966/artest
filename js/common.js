3cb8c15ee12a43e3bc6f61dad292061508093b21
nknhb966 <86583737+nknhb966@users.noreply.github.com>
2024-03-11 13:01:35 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -216,13 +216,13 @@ const init = () => {
   var lastTouchEnd = 0;
   document.addEventListener('touchstart', onTouchStart, { passive: false });
   document.addEventListener('touchmove', onTouchMove, { passive: false });
-  document.addEventListener('touchend', function (event) {
-    var now = (new Date()).getTime();
-    if (now - lastTouchEnd <= 300) {
-        event.preventDefault();
-    }
-    lastTouchEnd = now;
-  }, false);
+  // document.addEventListener('touchend', function (event) {
+  //   var now = (new Date()).getTime();
+  //   if (now - lastTouchEnd <= 300) {
+  //       event.preventDefault();
+  //   }
+  //   lastTouchEnd = now;
+  // }, false);
 };
 
 function onTouchStart(event) {
