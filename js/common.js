72f50686a3f6494012758f31e32e86f80a0a26d1
nknhb966 <86583737+nknhb966@users.noreply.github.com>
2024-03-13 11:55:08 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -290,8 +290,8 @@ function onTouchMove(event) {
                 const newX = deltaX * Math.cos(rad) - deltaY * Math.sin(rad);
                 const newY = deltaX * Math.sin(rad) + deltaY * Math.cos(rad);
     
-                model.object3D.position.x += newX * 0.01;
-                model.object3D.position.z += newY * 0.01;
+                model.object3D.position.x += newX * 0.005;
+                model.object3D.position.z += newY * 0.005;
     
                 touchStartX = touchX;
                 touchStartY = touchY;
