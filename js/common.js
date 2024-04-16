e740e0746003b4242fa03c07efb4a5713a44e417
nknhb966 <86583737+nknhb966@users.noreply.github.com>
2024-04-16 18:11:53 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -425,8 +425,7 @@ window.addEventListener("DOMContentLoaded", initOS);
 function initOS() {
     os = detectOSSimply();
     if (os == "iphone") {
-        permitDeviceOrientationForSafari();
-        // document.querySelector("#permit2").addEventListener("click", permitDeviceOrientationForSafari);
+        document.querySelector("#permit2").addEventListener("click", permitDeviceOrientationForSafari);
         window.addEventListener(
             "deviceorientation",
             handleOrientation,
